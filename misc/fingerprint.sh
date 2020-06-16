24da31e46eb2b2d179ce395248f68e2661d87b29
Oliver Kaufmann <okaufmann91@gmail.com>
2020-06-16 18:31:23 +0200

    wip

--- misc/fingerprint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-sudo chmod +w /etc/pam.d/sudo
-sudo vim /etc/pam.d/sudo
-
-echo 'ensure the following line'
-echo 'auth       sufficient     pam_tid.so'
-
-sudo chmod -w /etc/pam.d/sudo
